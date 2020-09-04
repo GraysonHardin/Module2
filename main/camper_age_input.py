66de3c7b996153e2b85e2eff86dc2ca4823b926c
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-09-03 22:11:07 -0500

    Completed assignment

--- main/camper_age_input.py
+++ main/camper_age_input.py
@@ -1,17 +1,21 @@
 def main():
-    age_in_years = int(input("Hi: "))
+    age_in_years = int(input("Enter age in years: "))
     age_in_months = convert_to_months(age_in_years)
     print(f'The age in months is {age_in_months}')
 
+    age_in_months = int(input("Enter age in months: "))
+    age_in_years = convert_to_years(age_in_months)
+    print(f'The age in years is {age_in_years}')
 
 
 def convert_to_months(age_in_years):
     age_in_months = age_in_years * 12
     return age_in_months
 
-def convert_to_years(age_in_months):
-    return age_in_months / 12
 
+def convert_to_years(age_in_months):
+    age_in_years = age_in_months / 12
+    return age_in_years
 
 
 if __name__ == '__main__':
--- test/test_functions.py
+++ test/test_functions.py
@@ -1,5 +1,4 @@
 import unittest
-from main import camper_age_input
 from main.camper_age_input import convert_to_months, convert_to_years
 
 
@@ -11,7 +10,6 @@ class FunctionTestCase(unittest.TestCase):
         actual = convert_to_months(user_input)
 
         self.assertEqual(expected, actual)
-
 
     def test__convert_to_months__given_two_year_return_twenty_four_months(self):
         user_input = 2
@@ -36,6 +34,7 @@ class FunctionTestCase(unittest.TestCase):
         actual = convert_to_months(user_input)
 
         self.assertEqual(expected, actual)
+
     def test__convert_to_months__given_five_year_return_sixty_months(self):
         user_input = 5
         expected = 60
@@ -52,19 +51,37 @@ class FunctionTestCase(unittest.TestCase):
 
         self.assertEqual(expected, actual)
 
-    def test__convert_to_years__given_twenty_four_return_two_year(self):
+    def test__convert_to_years__given_twenty_four_months_return_two_year(self):
         user_input = 24
         expected = 2
 
         actual = convert_to_years(user_input)
 
         self.assertEqual(expected, actual)
-    
 
+    def test__convert_to_years__given_thirty_six_months_return_three_year(self):
+        user_input = 36
+        expected = 3
 
+        actual = convert_to_years(user_input)
 
+        self.assertEqual(expected, actual)
 
+    def test__convert_to_years__given_forty_eight_return_four_year(self):
+        user_input = 48
+        expected = 4
 
+        actual = convert_to_years(user_input)
+
+        self.assertEqual(expected, actual)
+
+    def test__convert_to_years__given_sixty_months_return_five_year(self):
+        user_input = 60
+        expected = 5
+
+        actual = convert_to_years(user_input)
+
+        self.assertEqual(expected, actual)
 
 
 if __name__ == '__main__':
