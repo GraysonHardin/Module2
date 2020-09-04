97260d9e4c0df29b8e868a9137dc41835e2ecc79
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-09-04 11:11:43 -0500

    Finished hello_world function assignment.

---
+++ pep_8_assignment/hello_world.py
@@ -0,0 +1,16 @@
+"""
+Program: hello_world_function.py
+Author: Grayson Hardin
+Last date modified: 9/1/2020
+
+
+The program stores a message within a variable (hello_world) and returns the value.
+"""
+
+
+def hello_world():
+    print('Hello World!')
+
+
+if __name__ == '__main__':
+    hello_world()
