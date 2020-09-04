e0af9da94645d43ac3457a19b8a4a49fc5c8408f
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-09-04 11:11:17 -0500

    Finished variable assignment.

---
+++ variable_assignment/use_varibles.py
@@ -0,0 +1,8 @@
+import variables
+
+"""
+On line 7, I have utilized a literal string interpolation which allows the programmer to format and compile multiple functions on a single line. Helps organize and keep the code clean.
+"""
+
+print(f'{variables.QUANTITY} {variables.ITEM} {variables.COST}')
+
---
+++ variable_assignment/variables.py
@@ -0,0 +1,9 @@
+"""
+Program: variables.py
+Author: Grayson Hardin
+Last date modified: 9/4/2020
+"""
+QUANTITY = 10  # Allows programmer to specify the quantity for the program
+ITEM = "pizzas will cost"  # After quantity has been given, this variable acts as the item.
+COST = 15.00  # This is a decimal number, but in this case is used to represent the cost.
+
